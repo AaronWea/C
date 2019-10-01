3fa74c1ea7a9c2ed92c03cc6da5224d55f1d310d
AaronWea <55831454+AaronWea@users.noreply.github.com>
2019-10-01 14:16:44 +0100

    Add files via upload

---
+++ ifStatements.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ifStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Is the weather sunny today?");
+
+            string sWeather = Console.ReadLine();
+
+            if (sWeather == "yes")
+            {
+                Console.WriteLine("Have fun");
+            }
+            else
+            {
+                Console.WriteLine("Unlucky");
+            }
+
+
+            Console.WriteLine("What was your test score?");
+
+            int Score = int.Parse(Console.ReadLine());
+
+            if (Score <= 25) 
+            {
+                Console.WriteLine("You failed.");
+            }
+            else if (Score >= 25)
+            {
+                Console.WriteLine("You passed!");
+            }
+
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
